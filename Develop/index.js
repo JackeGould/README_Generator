2cb3dacbcbf0e42d0fc8ad8917dd6afd0a8d921e
JackeGould <jmgould913@gmail.com>
2024-03-01 02:20:21 -0500

    Changes made to README.md

--- Develop/index.js
+++ Develop/index.js
@@ -1,11 +1,3 @@
-// # ${title}
-// ## ${description}
-// ## ${installation}
-// ## ${usage}
-// ## ${contribution}
-// ## ${test}
-// ## ${license}`;
-
 const inquirer = require('inquirer');
 
 const { writeFile } = require('fs').promises;
@@ -63,8 +55,6 @@ const promptUser = () => {
 
 const generateReadMe = ({ title, description, installation, usage, contribution, test, license, github, email }) =>
     `
-    ![badge](https://img.shields.io/badge/license-${license}-brightgreen)<br />
-
   # ${title}
 
   ## Description
@@ -97,17 +87,15 @@ const generateReadMe = ({ title, description, installation, usage, contribution,
   
   ## License 
   
-  ![badge](https://img.shields.io/badge/license-${license}-brightgreen)
-<br />
-This application is covered by the ${license} license. 
+  This application is covered by the ${license}. 
   
   ## Questions 
 
   If you have any additional questions regarding this respository, please feel free to reach me via the email address provided below.
   
-  ${github}
-  
   ${email}
+
+  Find me on GitHub: [${github}](https://github.com/${github})
   
   `;
 
@@ -120,36 +108,10 @@ const init = () => {
         .catch((err) => console.error(err));
 };
 
-function renderLicenseBadge(license) {
-    if (license) {
-        return `![License](https://img.shields.io/badge/license-${license}-blue.svg)`;
-    } else {
-        return '';
-    }
-}
 
 init();
 
 
+// I was able to write all of my code in one file but cannot figure out the badges.
 
-
-// WHEN I enter my project title
-// THEN this is displayed as the title of the README
-// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
-// WHEN I choose a license for my application from a list of options
-// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-// WHEN I enter my GitHub username
-// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-// WHEN I enter my email address
-// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
-// WHEN I click on the links in the Table of Contents
-// THEN I am taken to the corresponding section of the README
-
-// DONE Uses the Inquirer package 
-// DONE Reformat the ReadMe file so its not in an HTML format
-
-// Does Table of Contents require user input/choice???
-// What am I supposed to be doing with my email and GitHub?? Is it my email/github or the users ??? Im assuming the users.
-// Add badges to generate markdown js file
-// Store as images
+// The example read me is being added inside of Develop, but i would prefer it be generated outside of Develop, and the real ReadMe.md be added to assets
---
+++ Develop/readme.md
@@ -0,0 +1,44 @@
+
+  # Professional README Generator
+
+  ## Description
+
+  A node.js application that uses user input from inquirer to populate a README.md file for a users repository.
+
+  # Table of Contents 
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contribution](#contribution)
+* [Test](#test)
+* [License](#license)
+* [Questions](#questions)
+  
+  ## Installation 
+  
+  npm install inquirer is required.
+  
+  ## Usage 
+  
+  Run the following command at the root of your project and answer the prompted questions: node index.js
+  
+  ## Contribution 
+  
+  Fork the repository, push changes and await approval.
+  
+  ## Test 
+  
+  No tests are included.
+  
+  ## License 
+  
+  This application is covered by the MIT License. 
+  
+  ## Questions 
+
+  If you have any additional questions regarding this respository, please feel free to reach me via the email address provided below.
+  
+  Jmgould913@gmail.com
+
+  Find me on GitHub: [Jackegould](https://github.com/Jackegould)
+  
+  

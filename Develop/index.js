e318b66feb995babf839c685e2635d4e67bdc8e7
JackeGould <jmgould913@gmail.com>
2024-02-29 21:26:32 -0500

    most modifications made to index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,147 @@
-// TODO: Include packages needed for this application
+// # ${title}
+// ## ${description}
+// ## ${installation}
+// ## ${usage}
+// ## ${contribution}
+// ## ${test}
+// ## ${license}`;
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const inquirer = require('inquirer');
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const { writeFile } = require('fs').promises;
 
-// TODO: Create a function to initialize app
-function init() {}
+const promptUser = () => {
+    return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is your READEME title?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please enter a description.',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'Please enter your installation instructions.',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Please enter your usage information.',
+      },
+      {
+        type: 'input',
+        name: 'contribution',
+        message: 'Please enter your contribution guidelines.',
+      },
+      {
+        type: 'input',
+        name: 'test',
+        message: 'Please enter your test instructions',
+      },
+    {
+      type: 'list',
+      message: 'Please choose a license.',
+      name: 'license',
+      choices: ['MIT License', 'The Unlicense', 'Mozilla Public License 2.0'],
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Please add your GitHub username.',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Please enter your email address',
+      },
+]);
+};
 
-// Function call to initialize app
-init();
+  const generateReadMe = ({ title, description, installation, usage, contribution, test, license, github, email }) =>
+  `
+  [![License](https://img.shields.io/badge/license-${license}-brightgreen)](https://opensource.org/licenses/${license})<br />
+
+  # ${title}
+
+  ## Description
+
+  ${description}
+
+  # Table of Contents 
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contribution](#contribution)
+* [Test](#test)
+* [License](#license)
+* [Questions](#questions)
+  
+  ## Installation 
+  
+  ${installation}
+  
+  ## Usage 
+  
+  ${usage}
+  
+  ## Contribution 
+  
+  ${contribution}
+  
+  ## Test 
+  
+  ${test}
+  
+  ## License 
+  
+  [![License](https://img.shields.io/badge/license-${license}-brightgreen)](https://opensource.org/licenses/${license})
+  <br />
+  This application is covered by the [${license}](https://opensource.org/licenses/${license}) license.
+  
+  ## Questions 
+
+  If you have any additional questions regarding this respository, please feel free to reach me via the email address provided above.
+  
+  ${github}
+  
+  ${email}
+  
+  `;
+
+  const init = () => {
+    promptUser()
+      // Use writeFile method imported from fs.promises to use promises instead of
+      // a callback function
+      .then((answers) => writeFile('readme.md', generateReadMe(answers)))
+      .then(() => console.log('Successfully wrote to readme.md'))
+      .catch((err) => console.error(err));
+  };
+  
+  init();
+
+
+
+
+// WHEN I enter my project title
+// THEN this is displayed as the title of the README
+// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+// WHEN I choose a license for my application from a list of options
+// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+// WHEN I enter my GitHub username
+// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+// WHEN I enter my email address
+// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+// WHEN I click on the links in the Table of Contents
+// THEN I am taken to the corresponding section of the README
+
+// DONE Uses the Inquirer package 
+// DONE Reformat the ReadMe file so its not in an HTML format
+
+// Does Table of Contents require user input/choice???
+// What am I supposed to be doing with my email and GitHub?? Is it my email/github or the users ??? Im assuming the users.
+// Add badges to generate markdown js file
+// Store as images
---
+++ Develop/readme.md
@@ -0,0 +1,48 @@
+
+  [![License](https://img.shields.io/badge/license-MIT License-brightgreen)](https://opensource.org/licenses/MIT License)<br />
+
+  # TEST
+
+  ## Description
+
+  t
+
+  # Table of Contents 
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contribution](#contribution)
+* [Test](#test)
+* [License](#license)
+* [Questions](#questions)
+  
+  ## Installation 
+  
+  t
+  
+  ## Usage 
+  
+  t
+  
+  ## Contribution 
+  
+  t
+  
+  ## Test 
+  
+  t
+  
+  ## License 
+  
+  [![License](https://img.shields.io/badge/license-MIT License-brightgreen)](https://opensource.org/licenses/MIT License)
+  <br />
+  This application is covered by the [MIT License](https://opensource.org/licenses/MIT License) license.
+  
+  ## Questions 
+
+  If you have any additional questions regarding this respository, please feel free to reach me via the email address provided above.
+  
+  JackeGould
+  
+  Jmgould913@gmail.com
+  
+  
